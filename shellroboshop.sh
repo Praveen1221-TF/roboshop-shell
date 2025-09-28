8b4071bcd5d051dd987aeceef75b38d7c662e406
praveen <praveensunnam1221@gmail.com>
2025-09-28 13:51:49 +0530

    instance creation

--- shellroboshop.sh
+++ shellroboshop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-AMI_ID='ami-09c813fb71547fc4f'
-SG_ID='sg-08223f8b4a8582c4a'
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-08223f8b4a8582c4a"
+ZONE_ID="Z0525756Y7HMXRRYAT2T"
+DOMAIN_NAME="practicedev.shop"
 
 for instance in $@
 
@@ -10,10 +12,31 @@ do
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
 
    echo "$instance : $IP" 
            
-done
+
+aws route53 change-resource-record-sets \
+  --hosted-zone-id Z0525756Y7HMXRRYAT2T	 \
+  --change-batch '
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
+  done
