7769e5b90dd7c182b417be68c042daa5de554ce1
praveen <praveensunnam1221@gmail.com>
2025-09-29 14:33:41 +0530

    changes done

--- catalogue.sh
+++ catalogue.sh
@@ -41,7 +41,7 @@ if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOG_FILE
     VALIDATE $? "Adding system User"
 else        
-    echo "$G skipping....$N"
+    echo -e "$G skipping....$N"
 fi        
 
 mkdir -p /app &>>$LOG_FILE
