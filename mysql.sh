919cd5bf076ca8e2258fb535f380ee4c28a35e3f
Praveen1221-TF <praveensunnam123@gmail.com>
2025-09-30 14:52:41 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,41 @@
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$(basename "$0" .sh)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
+
+mkdir -p $LOGS_FOLDER
+echo "script started excuted at : $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then        
+    echo "Error: please run the script with root user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R .... $2  failure $N" | tee -a $LOG_FILE
+         exit 1
+    else        
+        echo -e "$G .... $2 success $N" | tee -a $LOG_FILE
+
+    fi
+}   
+
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Intalled mysql"
+
+systemctl enable msysql &>>$LOG_FILE
+VALIDATE $? "Enable mysql"
+
+systemctl start msysql &>>$LOG_FILE
+VALIDATE $? "Start mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Settingup root password"
